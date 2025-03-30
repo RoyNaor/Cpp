736c347b767d28025d21d476b8daa3cf637d605a
Roy_Naor <133534625+RoyNaor@users.noreply.github.com>
2025-03-30 11:52:44 +0300

    Add files via upload

---
+++ Ex1_New/Algorithms.hpp
@@ -0,0 +1,24 @@
+//
+// Created by royna on 26/03/2025.
+//
+
+#ifndef EX1_NEW_ALGORITHMS_HPP
+#define EX1_NEW_ALGORITHMS_HPP
+
+#include "Graph.hpp"
+
+namespace algorithms {
+
+    class Algorithms {
+        public:
+            static graph::Graph bfs(const graph::Graph& graph, int source);
+            static graph::Graph dfs(const graph::Graph& graph, int source);
+            static graph::Graph dijkstra(const graph::Graph& graph, int source);
+            static graph::Graph prim(const graph::Graph& graph);
+            static graph::Graph kruskal(const graph::Graph& graph);
+    };
+
+} // namespace Algorithms
+
+
+#endif //EX1_NEW_ALGORITHMS_HPP
